ad58efb890de84105914c83d7758ded49544b72d
Konstantin Albatov <albatovkonstantin@yandex.ru>
2025-10-09 11:48:18 +0300

    Lexture 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,80 +1,91 @@
 package org.example;
 
-import java.util.Objects;
-
-class Container<T> implements Containable<T> {
-    private T data;
-
-    public Container(T data) {
-        this.data = data;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-}
-
-class Pair<K, V> {
-    K key;
-    V value;
-}
-
-interface Containable<T> {
-    void setData(T value);
-    T getData();
-}
-
-class StringContainer implements Containable<String> {
-    @Override
-    public void setData(String value) {
-
-    }
-
-    @Override
-    public String getData() {
-        return null;
-    }
-}
-
-class Car implements Comparable<Car> {
-    String model;
-
-    @Override
-    public int compareTo(Car o) {
-        return model.compareTo(o.model);
-    }
-
-    public Car(String model) {
-        this.model = model;
-    }
-}
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    
+
+    public static void main(String[] args) throws IOException, NoSuchMethodException {
+//        File file = new File("a.txt");
+//        try {
+//            boolean result = file.createNewFile();
+//            System.out.println(result);
+//
+//            System.out.println(file.getName());
+//            System.out.println(file.getAbsoluteFile());
+//            System.out.println(file.length());
+//            System.out.println(file.isDirectory());
+//            System.out.println(file.lastModified());
+//
+//            File dir = new File("directory");
+//            result = dir.mkdir();
+//            System.out.println(result);
+//            System.out.println(dir.isDirectory());
+//
+//            File src = new File("src");
+//            for (File f: src.listFiles()) {
+//                System.out.println(f.getName());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try (BufferedReader out = new BufferedReader(new FileReader("a.txt"));
+//             FileWriter in = new FileWriter("a.txt")) {
+//            String text = "Hello, world!";
+//            in.write(text);
+//            in.append("\nSecondLine");
+//            in.flush();
+//
+//            String s;
+//            while ((s = out.readLine()) != null) {
+//                System.out.println(s);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println();
+//        Scanner scanner = new Scanner(System.in);
+
+
+//        Files.readAllLines(Path.of(".../..."));
+//        Path path = Path.of("..");
+//        path.toAbsolutePath();
+//        Files.copy();
+//        Files.createTempFile();
+
+
+
+        Sample obj = new Sample("Java Reflection API");
+
+        Class<? extends Sample> cls = obj.getClass();
+        System.out.println(cls.getName());
+
+        Constructor<? extends Sample> constructor = cls.getConstructor(String.class);
+        System.out.println(constructor.getName());
+
+    }
+}
+
+class Sample {
+    private final String str;
+
+    public Sample(String str) {
+        this.str = str;
     }
 
-    public static <T> T findFirst(T[] array) {
-        if (array.length == 0) {
-            return null;
-        }
-        return array[0];
+    public void printStr() {
+        System.out.println(str);
     }
 
-    public static <T extends Comparable<T>> T findMax(T[] array) {
-        if (array.length == 0) {
-            return null;
-        }
-        T max = array[0];
-        for (T obj: array) {
-            if (obj.compareTo(max) > 0) {
-                max = obj;
-            }
-        }
-        return max;
+    private void privateMethod() {
+        System.out.println("Private method");
     }
 }
